994eb248eb6a73d6fbf87195eb6a149f462c99df
AbeMasaya <baskeno777@yahoo.co.jp>
2018-10-01 22:43:15 +0900

    10/01 19:00

--- Assets/Photon/PhotonUnityNetworking/Resources/manage.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/manage.cs
@@ -15,7 +15,10 @@ public class manage : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        if (Input.GetMouseButtonDown(0) && keyLock) {
+        if(Input.GetKeyDown(KeyCode.A)){
+            Connect();
+        }
+        if (Input.GetMouseButtonDown(0)) {
             Debug.Log("オブジェクト生成成功");
             GameObject mySyncObj = PhotonNetwork.Instantiate("Cube", new Vector3(9.0f, 0f, 0f), Quaternion.identity, 0);
             Rigidbody mySyncObjRB = mySyncObj.GetComponent<Rigidbody>();
@@ -32,29 +35,22 @@ public class manage : MonoBehaviour {
     void Connect(){
         if(PhotonNetwork.IsConnected){
             //PhotonCloudサーバーに接続されている場合、サーバー内のどこかのルームに入室する
-            Debug.Log("ルームに入室しました。");
-            PhotonNetwork.JoinRandomRoom();
-            keyLock = true;
+            if(PhotonNetwork.JoinRandomRoom()){
+                Debug.Log("ルームに入室しました。");
+                keyLock = true;
+            }else{
+                Debug.Log("ルームの入室に失敗しました。");
+            }
+
         } else {
             //PhotonCloudサーバーと未接続の場合、「PhotonServerSettings」に設定通りにサーバーに接続する
             Debug.Log("サーバーに接続されていません。再接続します。");
             PhotonNetwork.GameVersion = gameVersion;
-            PhotonNetwork.ConnectUsingSettings();
-            if (true) {
-                Debug.Log("接続に成功しました。");
-                PhotonNetwork.JoinRandomRoom();
-                keyLock = true;
-                Debug.Log("ルームに入室しました。");
-            } else {
-                Debug.Log("接続に失敗しました。");
+            if(PhotonNetwork.ConnectUsingSettings()){
+                Debug.Log("サーバーに接続成功。ロビーに入室しました。");
+            }else{
+                Debug.Log("サーバーとの接続に失敗しました。");
             }
         }
-    }
-
-    public void OnCreatedRoom() {
-        Debug.Log("room created.");
-    }
-    public void OnJoinedRoom() {
-        Debug.Log("joined room.");
     }
 }
---
+++ Assets/Photon/PhotonUnityNetworking/Resources/test.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class test : MonoBehaviour {
+    #region Private Serializable Fields
+
+
+    #endregion
+
+
+    #region Private Fields
+
+
+    /// <summary>
+    /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+    /// </summary>
+    string gameVersion = "1";
+
+
+    #endregion
+
+
+    #region MonoBehaviour CallBacks
+
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+    /// </summary>
+    void Awake() {
+        // #Critical
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+    /// </summary>
+    void Start() {
+        Connect();
+    }
+
+    private void FixedUpdate() {
+        if(Input.GetKeyDown(KeyCode.A)){
+            Connect();
+        }
+    }
+
+
+    #endregion
+
+
+    #region Public Methods
+
+
+    /// <summary>
+    /// Start the connection process.
+    /// - If already connected, we attempt joining a random room
+    /// - if not yet connected, Connect this application instance to Photon Cloud Network
+    /// </summary>
+    public void Connect() {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected) {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+            Debug.Log("ルーム入室");
+        } else {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+            Debug.Log("ロビー入室");
+        }
+    }
+
+
+    #endregion
+
+
+}
